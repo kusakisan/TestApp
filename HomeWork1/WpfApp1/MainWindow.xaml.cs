bf12a8fbf60778acda472dff5169f64710506d66
kusakisan <transparent.transparent@mail.ru>
2021-04-27 23:30:47 +0300

    init

---
+++ HomeWork1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckPassword_Checked(object sender, RoutedEventArgs e)
+        {
+            TextBoxPassword.Text = PasswordBox.Password;
+            TextBoxPassword.Visibility = Visibility.Visible;
+            PasswordBox.Visibility = Visibility.Hidden;
+        }
+        private void CheckPassword_Unchecked(object sender, RoutedEventArgs e)
+        {
+            PasswordBox.Password = TextBoxPassword.Text;
+            PasswordBox.Visibility = Visibility.Visible;
+            TextBoxPassword.Visibility = Visibility.Hidden;
+        }
+
+        public void Proverka()
+        {
+            if (TextBoxLogin.Text == "login1" && (PasswordBox.Password == "tuptup" || TextBoxPassword.Text == "tuptup"))
+            {
+                WindowZakaz zakaz = new WindowZakaz();
+                zakaz.Show();
+            }
+            ////if (TextBoxLogin.Text != "login1" && (PasswordBox.Password != "tuptup" || TextBoxPassword.Text != "tuptup"))
+            ////{
+            ////    MessageBox.Show( "Проверьте введёные данные, возможно включен Caps Lock","Неверно введены данные", MessageBoxButton.OK, MessageBoxImage.Warning);
+            ////}
+
+            else if (TextBoxLogin.Text != "login1")
+            {
+                MessageBox.Show("Вы не ввели логин или он введён неверно!", "Заполните все поля", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            if (TextBoxLogin.Text != "tuptup" && PasswordBox.Password != "tuptup")
+            {
+                MessageBox.Show("Вы не ввели пароль или он введён неверно!", "Заполните все поля", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void ButtonAuthorizited_Click(object sender, RoutedEventArgs e)
+        {
+            Proverka();
+        }
+    }
+}
---
+++ HomeWork1/WpfApp1/WindowZakaz.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowZakaz.xaml
+    /// </summary>
+    public partial class WindowZakaz : Window
+    {
+        public WindowZakaz()
+        {
+            InitializeComponent();
+            DataGridZakaz.ItemsSource = HomeTaskEntities.GetContext().Users.ToList();
+        }
+
+        private void ButtonNewZakaz_Click(object sender, RoutedEventArgs e)
+        {
+            RegistrZakaza registrZakaza = new RegistrZakaza();
+            registrZakaza.Show();
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
